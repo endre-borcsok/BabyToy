d7413f1724eaa2cd7a8daa4b6db6e501e5e68cb9
DESKTOP-DJSTJIS\Endre <endre16@gmail.com>
2017-04-09 00:38:04 +0100

    Crashes made safer.

---
+++ app/src/main/java/com/ebsoft/babytoy/BabyToyApplication.java
@@ -0,0 +1,30 @@
+package com.ebsoft.babytoy;
+
+import android.app.AlarmManager;
+import android.app.Application;
+import android.content.Intent;
+
+import static java.security.AccessController.getContext;
+
+/**
+ * Created by Endre on 09/04/2017.
+ */
+
+public class BabyToyApplication extends Application {
+
+    private Thread.UncaughtExceptionHandler mUnCaughtExceptionHandler =
+            new Thread.UncaughtExceptionHandler() {
+                @Override
+                public void uncaughtException(Thread thread, Throwable ex) {
+                    Intent intent = new Intent(BabyToyApplication.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    System.exit(2);
+                }
+            };
+
+    public BabyToyApplication() {
+        // setup handler for uncaught exception
+        Thread.setDefaultUncaughtExceptionHandler(mUnCaughtExceptionHandler);
+    }
+}
